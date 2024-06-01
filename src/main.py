f4368ee24629ba0e3fe4457a06bd2d2dbab32c78
Denis Anufriev <wenomavel89@gmail.com>
2024-06-01 19:32:00 +0300

    основная функция, добавил комментарии

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
-from func import hide_symbols
+import func
 
 
 def main():
-    """
-    main operations 12
-    """
-    operation = hide_symbols()
-    # print(operation[0:])
-    for i in operation[:5]:
+    # Загрузка операций из файла
+    operations_dict = func.get_operations("../operations.json")
+    # Сортировка операций по дате и времени
+    sorted_operations = func.sort_day_time(operations_dict)
+    # Маскировка номеров карт и счетов
+    hide_symbols = func.hide_symbols(sorted_operations, "*", 4)
+
+    # Вывод первых 5 операций
+    for i in hide_symbols[:5]:
         if 'from' in i:
             print(f"""{i['date'][:10]} {i['description']}
 {i['from']} -> {i['to']}
